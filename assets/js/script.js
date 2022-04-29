0621894d52b6f875f4e03b98d6d769ad174c589f
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2022-04-29 15:21:57 -0300

    Added hidden divs and js

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+function seeMore() {
+  document.getElementById('seeMore').style.display = 'inline'
+  document.getElementById('seeMoreButton').style.display = 'none'
+}
+
+function hideText() {
+  document.getElementById('seeMore').style.display = 'none'
+  document.getElementById('seeMoreButton').style.display = 'inline'
+}
+
+function seeMoreAnswers() {
+  document.getElementById('moreAnswers').style.display = 'flex'
+}
+
+function hideMoreAnswers() {
+  document.getElementById('moreAnswers').style.display = 'none'
+}
+
+function newTopic() {
+  document.getElementById('form').style.display = 'block'
+  document.getElementById('ideas').style.display = 'none'
+  document.getElementById('newTopicSuccess').style.display = 'none'
+}
+
+function newComment() {
+  document.getElementById('newTopicSuccess').style.display = 'flex'
+  document.getElementById('newTopic').style.display = 'block'
+  document.getElementById('form').style.display = 'none'
+}
